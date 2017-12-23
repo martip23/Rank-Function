101eb556c08b098b3c748ff590eac3b2c55eeaa4
martip23 <patrickmartinez77@gmail.com>
2017-12-22 18:49:27 -0600

    first commit

---
+++ src/utilities/Positroids.java
@@ -0,0 +1,26 @@
+package utilities;
+
+/**
+ * Utility library to hold the functions that are generally used to find the
+ * rank of an interval or sets of intervals of a Matroid.
+ * @author martip23
+ * December 21, 2017
+ */
+public class Positroids {
+	
+	public static int galeCompare (int a, int b, int i) {
+		return 0;
+	}
+	
+	public static int grassmanNecklace() {
+		return 0;
+	}
+	
+	public static int positroidBasis () {
+		return 0;
+	}
+	
+	public static int rankFind () {
+		return 0;
+	}
+}
